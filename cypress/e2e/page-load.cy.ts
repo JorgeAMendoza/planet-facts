cfb3b6880ab6eb6feb74ade1919c590c948c7dfb
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-01-28 11:48:25 -0600

    create test suite for when the page loads

--- cypress/e2e/page-load.cy.ts
+++ cypress/e2e/page-load.cy.ts
@@ -3,9 +3,25 @@
 describe("on first page load", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.get('[data-cy="planetName"]').as("planetName");
+    cy.get('[data-cy="planeImage"]').as("planetImage");
+    cy.get('[data-cy="overviewContentButton"]').as("overviewContentButton");
   });
 
-  it("just checking if this works!", () => {
-    cy.get("p");
+  it("inital redirect goes to the earth page", () => {
+    cy.location("pathname").should("contain", "earth");
+  });
+
+  it("page information displayed with default content", () => {
+    cy.get("@planetName").should("contain.text", "earth");
+    cy.get("@planetImage")
+      .should("have.attr", "src")
+      .should("contain", "earth");
+    cy.get("@overviewContentButton").should(
+      "have.css",
+      "background-color",
+      "#6D2ED5"
+    );
   });
 });
+ 
