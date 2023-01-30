1b7762f13f2bc0154ea5b056e78cd9fd09a5d2aa
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-01-30 05:18:38 -0600

    create test suite for navigating to the other planet pages

---
+++ cypress/e2e/planet-switch.cy.ts
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+const planets = [
+  "Mercury",
+  "Venus",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
+  "Earth",
+];
+
+describe("navigate to different planet/different page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  for (const planet of planets) {
+    it(`navigate to planet ${planet}`, () => {
+      cy.get(`[data-cy=to${planet}Link]`).click();
+      cy.location("pathname").should("contain", `planets/${planet}`);
+      cy.get('[data-cy="planetName"]').should("contain.text", planet);
+    });
+  }
+});
