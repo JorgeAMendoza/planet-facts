4ba9f2f35687a99c5b9ace305789b88f2c83dde9
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-02-03 07:24:52 -0600

    create structure for the mobile navigation menu, set a boolean which is toggled by the hamburger button

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,93 @@
+import { useState } from "react";
 import Link from "next/link";
 import styles from "./NavBar.module.css";
+import Image from "next/image";
+import hamburgerIcon from "../../../public/icon-hamburger.svg";
+import arrowIcon from "../../../public/icon-chevron.svg";
 
 const NavBar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
     <header className={styles.navBar}>
       <p>the planets</p>
 
+      {/* rendered on screens smaller than tablet */}
+      <button
+        aria-label="Click to open planet navigation menu"
+        onClick={() => setShowMobileMenu(!showMobileMenu)}
+      >
+        <Image src={hamburgerIcon} alt="" width={24} height={17}></Image>
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/planets/mercury" data-cy="toMercuryLink">
+              mercury{" "}
+              <span>
+                <Image src={arrowIcon} alt="" width={4} height={8} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/planets/venus" data-cy="toVenusLink">
+              venus{" "}
+              <span>
+                <Image src={arrowIcon} alt="" width={4} height={8} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/planets/earth" data-cy="toEarthLink">
+              earth{" "}
+              <span>
+                <Image src={arrowIcon} alt="" width={4} height={8} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/planets/mars" data-cy="toMarsLink">
+              mars{" "}
+              <span>
+                <Image src={arrowIcon} alt="" width={4} height={8} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/planets/jupiter" data-cy="toJupiterLink">
+              jupiter{" "}
+              <span>
+                <Image src={arrowIcon} alt="" width={4} height={8} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/planets/saturn" data-cy="toSaturnLink">
+              saturn{" "}
+              <span>
+                <Image src={arrowIcon} alt="" width={4} height={8} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/planets/uranus" data-cy="toUranusLink">
+              uranus{" "}
+              <span>
+                <Image src={arrowIcon} alt="" width={4} height={8} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/planets/neptune" data-cy="toUranusLink">
+              neptune{" "}
+              <span>
+                <Image src={arrowIcon} alt="" width={4} height={8} />
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* rendered on tablet and above */}
       <nav>
         <ul className={styles.navLinks}>
           <li>
